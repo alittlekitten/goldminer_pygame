2f253a27eb129e8d066f8f695c9a8b44ec070223
alittlekitten <dlscjs8646@gmail.com>
2021-06-26 03:33:28 +0900

    ver 1.0.5 충돌처리기본

---
+++ practice.py
@@ -0,0 +1,14 @@
+# 육십분법과 호도법 변환 연습
+import math
+
+rad_angle = math.radians(0)
+print(rad_angle) # 0.0
+
+rad_angle = math.radians(90)
+print(rad_angle) # 3.141592... / 2 = (1/2) * Pi
+
+rad_angle = math.radians(180)
+print(rad_angle) # 3.141592... = Pi
+
+rad_angle = math.radians(270)
+print(rad_angle) # 3.141592 * 3 / 2 = (3/2) * Pi
